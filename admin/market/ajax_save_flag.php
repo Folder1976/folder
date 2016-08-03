36a3da5518c4303bf19ee7fb72af6ccc5d2bede4
Sergey Lipsits <lipsits@yandex.ru>
2016-08-03 23:44:29 +0300

    Select varket tovars addon

--- admin/market/ajax_save_flag.php
+++ admin/market/ajax_save_flag.php
@@ -11,6 +11,20 @@ include '../config/config.php';
 
 global $folder;
 
+include "../class/class_product.php";
+$Product = new Product($folder);
+
+$sql = 'SELECT tovar_artkl FROM tbl_tovar WHERE tovar_id = \''.$TovID.'\';';
+$r = $folder->query($sql);
+$tmp = $r->fetch_assoc();
+
+$allBrothers = $Product->getProductBrotherOnArtikl($tmp["tovar_artkl"]);
+
+foreach($allBrothers as $tK => $tArr) {
+	if (!isset($tArr["tovar_id"]) || !is_numeric($tArr["tovar_id"])) {continue;}
+	$sql = 'UPDATE tbl_tovar SET `use_in_market`=' . $YS . ' WHERE tovar_id = \''.$tArr["tovar_id"].'\';';
+	$folder->query($sql) or die('error update_social ' .$sql);
+}
 
 $sql = 'UPDATE tbl_tovar SET `use_in_market`=' . $YS . ' WHERE tovar_id = \''.$TovID.'\';';
 
