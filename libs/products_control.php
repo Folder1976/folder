a5962c17d13670d05e84c539270ae9aceb8eeb0c
Folder <folder.list@gmail.com>
2017-02-02 13:59:02 +0200

    Новинки

--- libs/products_control.php
+++ libs/products_control.php
@@ -25,7 +25,7 @@ $timer[] = timer('Категории');
   
     $step = 15;
     if(isset($_GET['step'])) $step = mysqli_escape_string($folder, $_GET['step']);
-    //if($step > 100) $step = 100;
+    if($step > 100) $step = 100;
     
     $page = 1;
     if(isset($_GET['page'])) $page = mysqli_escape_string($folder, $_GET['page']);
@@ -193,9 +193,9 @@ $timer[] = timer('Категории');
                `tovar_inet_id` > 0 $brand_filter $attr_str
                GROUP BY tovar_name_1
                ORDER BY T.tovar_id DESC
-               LIMIT 0, 10000;
+               LIMIT 0, 1000;
                ";
-        //echo $sql;
+       // echo $sql;
         $getCount = $folder->query($sql);
         $count = $getCount->num_rows;
         if($getCount->num_rows > 0){
@@ -228,9 +228,9 @@ $timer[] = timer('Всего товаров');
 			   `tovar_last_edit_user`,
                `price_tovar_curr_".$setup['web default price']."` as curr1,
                `price_tovar_curr_".$_SESSION[BASE.'userprice']."` as curr2,
-               TSI.price_1,
+               /*TSI.price_1,*/
                sum(TSI.items) AS items,
-			    T.use_in_market
+               MIN(TSI.price_1) AS price_1
                FROM 
                `tbl_tovar` T
                 LEFT JOIN tbl_tovar_suppliers_items TSI ON TSI.tovar_id = T.tovar_id
@@ -243,7 +243,7 @@ $timer[] = timer('Всего товаров');
                GROUP BY tovar_name_1
                ORDER BY T.sort ASC, CASE items WHEN 0 THEN 1 ELSE 0 END ASC, price_1 ASC, `tovar_name_1` ASC
                LIMIT $start, $step";
-               
+               //echo $sql;
    }elseif($key == 'FIND'){
       $searchq = $id;
       $sql = "SELECT 	`tovar_inet_id_parent`,
@@ -255,9 +255,9 @@ $timer[] = timer('Всего товаров');
 			   `tovar_last_edit_user`,
                `price_tovar_curr_".$setup['web default price']."` as curr1,
                `price_tovar_curr_".$_SESSION[BASE.'userprice']."` as curr2,
-                TSI.price_1,
+               /* TSI.price_1,*/
                sum(TSI.items) AS items,
-			    T.use_in_market
+               MIN(TSI.price_1) AS price_1
                FROM 
                `tbl_tovar` T
                LEFT JOIN tbl_tovar_suppliers_items TSI ON TSI.tovar_id = T.tovar_id
@@ -284,20 +284,20 @@ $timer[] = timer('Всего товаров');
 			   `tovar_last_edit_user`,
 			   `price_tovar_curr_".$setup['web default price']."` as curr1,
                `price_tovar_curr_".$_SESSION[BASE.'userprice']."` as curr2,
-			   T.use_in_market
+               sum(TSI.items) AS items,
+               MIN(TSI.price_1) AS price_1
+               
                
                FROM 
                `tbl_tovar` T
+               LEFT JOIN tbl_tovar_suppliers_items TSI ON TSI.tovar_id = T.tovar_id
                LEFT JOIN tbl_price_tovar ON price_tovar_id = T.tovar_id
                $join_str
-               /*RIGHT JOIN tbl_attribute_to_tovar AT ON AT.tovar_id = T.tovar_id AND attribute_id = 2*/
                WHERE
                `tovar_inet_id` > 0 $brand_filter $attr_str
-               /*AND atribute_value = 'Черный'*/
                 GROUP BY tovar_name_1
-               ORDER BY T.tovar_id DESC
-               LIMIT $start, $step
-               /*LIMIT 0, $key*/
+               ORDER BY  T.tovar_id DESC, T.sort ASC, items DESC, price_1 ASC, `tovar_name_1` ASC
+             LIMIT $start, $step
              ";
    }
 //echo $sql;
@@ -412,81 +412,43 @@ $timer[] = timer('Получили продукты');
 			$products[$artkl]['color_variants']	= $Product->getColorVariants($products[$artkl]['model'],$products[$artkl]['color']);
 			 
 			 
-			//echo '<pre>'; print_r(var_dump($attribute_filter));         
-             //Ключ проверки товара по фильтру
+	         //Ключ проверки товара по фильтру
              $tovar_filtered = true;
 
-             //если есть ключи для фильтрации вовара
-             /*
-            if($no_filter == false AND !empty($attributes)){
-                foreach($attributes as $index => $value){
-                   //На атрибуты
-				   if($value['filter'] == 1){
-                      if(isset($filter[$index])){
-                         if(!array_key_exists($value['attribute_value'], $filter[$index])){
-                            $tovar_filtered = false;
-                         }
-                      }
-                   }
-				   
-                }
-            }*/
-        
-            /*
-            //Если не высталено фильтрование - уберем товары которые вообще без атрибутов
-            if($no_filter == false AND empty($attributes)){
-                $tovar_filtered = false;
-            } */   
-            /*
-			 //Если товар не прошел проверку на фильтр
-             if($tovar_filtered == false){
-                unset($products[$artkl]);
-                continue;
-             }
-            */  
-             //Массив пишем по ключу Артикл
-             //$products_info['parent'][$tmp['tovar_inet_id_parent']] = $tmp['tovar_inet_id_parent'];
-      
-             //Считаем количество артиклей в подкатегориях
+           //Считаем количество артиклей в подкатегориях
              if(!isset($artikles[$artkl])){
-                /*в аяксе теперь
-                if(isset($products_parent_items[$tmp['tovar_inet_id_parent']])){
-                   $products_parent_items[$tmp['tovar_inet_id_parent']]++;   
-                }else{
-                   $products_parent_items[$tmp['tovar_inet_id_parent']] = 1;
-                }*/
                 $artikles[$artkl] = $artkl;
              }
-             
+  
+            $alias = $Alias->getProductAlias($tmp['tovar_id']);
              $products[$artkl]['name'] = $tmp['tovar_name'];
              $products[$artkl]['id'] = $tmp['tovar_id'];
-             $products[$artkl]['alias'] = HOST_URL.'/'.$Alias->getProductAlias($tmp['tovar_id']);
+             $products[$artkl]['alias'] = HOST_URL.'/'.$alias;
              $products[$artkl]['url'] = $products[$artkl]['alias'];
              $products[$artkl]['img'] = $Product->getProductPicOnArtkl($artkl);
 			 $products[$artkl]['social'] = $Product->getProductSocial($artkl);
 			  //$products[$artkl]['memo'] = $Product->getProductMemoShort($tmp['tovar_id']);
              $products[$artkl]['size'][$size]['id'] = $tmp['tovar_id'];
              $products[$artkl]['size'][$size]['size'] = $size;
-             $products[$artkl]['size'][$size]['price'] = $Product->getProductPrice($tmp['tovar_id']);
+             $products[$artkl]['size'][$size]['price'] = $tmp['price_1'];//$Product->getProductPrice($tmp['tovar_id']);
              $products[$artkl]['size'][$size]['curr'] = $tmp['curr2'];
-             $products[$artkl]['ymark'] = $tmp['use_in_market'];
-             $products[$artkl]['size'][$size]['items'] = $Product->getProductOnWare($tmp['tovar_id']);
-             
+             $products[$artkl]['size'][$size]['items'] = $tmp['items'];//$Product->getProductOnWare($tmp['tovar_id']);
+      
              //Будем брать описание если оно не пустое.
-             $tmp_memo = $Product->getProductMemoShort($tmp['tovar_id']);
+             $tmp_memo = $Product->getProductMemoShort($alias);
              if($tmp_memo != ''){
                 $products[$artkl]['memo']  = $tmp_memo;
              }
              
              //Общее количество
              if(isset($products[$artkl]['total'])){
-                $products[$artkl]['total'] += $Product->getProductOnWare($tmp['tovar_id']);
+                $products[$artkl]['total'] += $tmp['items'];//$Product->getProductOnWare($tmp['tovar_id']);
              }else{
-                $products[$artkl]['total'] = $Product->getProductOnWare($tmp['tovar_id']);
+                $products[$artkl]['total'] = $tmp['items'];//$Product->getProductOnWare($tmp['tovar_id']);
              }
              
              //Наименьшая цена на морду 
-             $tmp_price = $Product->getProductPrice($tmp['tovar_id']);
+             $tmp_price = $tmp['price_1']; //$Product->getProductPrice($tmp['tovar_id']);
 			  if($tmp_price > 0){
 			  if($max_price < $tmp_price) $max_price = $tmp_price;
 			  
@@ -496,7 +458,7 @@ $timer[] = timer('Получили продукты');
 			  if(isset($products[$artkl]['price']) AND $tmp_price < $products[$artkl]['price']){
 				 $products[$artkl]['price'] = $tmp_price;
 				 $products[$artkl]['curr'] = $tmp['curr2'];
-			  }else{
+			  }elseif(!isset($products[$artkl]['price'])){
 				 $products[$artkl]['price'] = $tmp_price;
 				 $products[$artkl]['curr'] = $tmp['curr2'];
 			  }
@@ -537,48 +499,7 @@ $timer[] = timer('Отсутствующие вниз');
           //Общий прогон по товарам
           $brands = array();
           $country = array();
-          /*Перенесено в аякс
-          foreach($products as $artkl => $product){
-                //Создаем массив брендов и стран
-                $tmp = $Brand->getBrandCodeOnProductArtkl($artkl);
-             
-			 	if($tmp){
-				  $brands[$tmp['brand_id']] = $tmp['brand_name'];
-				  $country[$tmp['country_id']] = $tmp['CountryName'];
-                
-				  //Фильтр брендов и страны
-				  //Если вообще выбрано сорт по бренду
-				  if(isset($_GET['brand'])){
-					 //Если сортировка по брендлу идет - то проверим выбран ли он (бренд)
-					 if(!isset($_GET['brand'][$tmp['brand_id']])){
-					   unset($products[$artkl]);
-					   continue;
-					 }
-				  }
-				  //Если вообще выбрано сорт по стране
-				  if(isset($_GET['country'])){
-					 //Если сортировка по стране идет - то проверим выбран ли он (страна)
-					 if(!isset($_GET['country'][$tmp['country_id']])){
-					   unset($products[$artkl]);
-					   continue;
-					 }
-				  }
-				 	
-				  $products[$artkl]['brand_country'] = $tmp['brand_name'].', '.$tmp['CountryName'];
-				  
-				}
-          }
-          $timer[] = timer('Общий прогон по товарам');
-          */
-
-/*  Теперь это по аяксу
-          //Возмем подкаталоги
-          if(isset($products_info)){
-             $products_info['parent'] = $Category->getCategoriesInfo($products_info['parent']);
-          }
-          //echo "<pre>"; print_r(var_dump($products)); 
-$timer[] = timer('Получили подкатегории');             
-*/           
+    
         //Уберем фильтры которые имеют по одному варианту
         foreach($attribute_filter as $index => $value){
              if(count($value['value']) < 2){
